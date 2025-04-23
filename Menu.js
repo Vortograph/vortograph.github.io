e56bb1baa5f51b1d589a16de7f43df1abdbe27fc
Vortograph <118545599+Vortograph@users.noreply.github.com>
2025-04-23 11:02:28 -0400

    Update Menu.js

--- Menu.js
+++ Menu.js
@@ -15,3 +15,30 @@ menuButton.addEventListener('click', function(event) {
 });
 
 
+//just some fun
+document.addEventListener('keydown', () => {
+    const konamiCode = [
+      'ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 
+      'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 
+      'KeyB', 'KeyA', 'Enter'
+    ];
+    
+    let currentCodeIndex = 0;
+    
+    document.addEventListener('keydown', (event) => {
+      const key = event.code;
+      
+      if (key === konamiCode[currentCodeIndex]) {
+        currentCodeIndex++;
+        
+        if (currentCodeIndex === konamiCode.length) {
+            window.location.href = 'colors.html'; // Redirect to colors.html
+          currentCodeIndex = 0; // Reset the code sequence after successful input
+        }
+      } else {
+        currentCodeIndex = 0; // Reset if the key sequence is incorrect
+      }
+    });
+  });
+
+
